592fbb9b14daf22dfb643d6e2e45f5786363a119
Shyamala Gowri Ganesan <shyamalagarun@gmail.com>
2022-06-13 16:41:55 -0400

    Aged Brie elixir changes

--- elixir/lib/gilded_rose.ex
+++ elixir/lib/gilded_rose.ex
@@ -3,11 +3,38 @@ defmodule GildedRose do
   # update_quality([%Item{name: "Backstage passes to a TAFKAL80ETC concert", sell_in: 9, quality: 1}])
   # => [%Item{name: "Backstage passes to a TAFKAL80ETC concert", sell_in: 8, quality: 3}]
 
+  @aged_brie "Aged Brie"
+  @backstage_passes "Backstage passes to a TAFKAL80ETC concert"
+  @sulfuras "Sulfuras, Hand of Ragnaros"
+  @conjured "Conjured"
+  @max_quality_default 50
+  @max_quality_sulfuras 80
+
+  defguardp is_aged_brie(item) when item.name === @aged_brie
+  defguardp is_backstage_passes(item) when item.name === @backstage_passes
+  defguardp is_sulfuras(item) when item.name === @sulfuras
+  defguardp is_conjured(item) when item.name === @conjured
+
   def update_quality(items) do
     Enum.map(items, &update_item/1)
+    |> IO.inspect
   end
 
-  def update_item(item) do
+  def update_item(item) when is_aged_brie(item) do
+    cond do
+      item.quality < 50 ->
+        %{item | quality: item.quality + 1}
+      true ->
+        item
+    end
+      |> update_sell_in 
+  end
+
+  defp update_sell_in(item) do
+    %{item | sell_in: item.sell_in - 1}
+  end
+
+  def update_item_original(item) do
     item = cond do
       item.name != "Aged Brie" && item.name != "Backstage passes to a TAFKAL80ETC concert" ->
         if item.quality > 0 do
--- elixir/test/gilded_rose_test.exs
+++ elixir/test/gilded_rose_test.exs
@@ -1,10 +1,24 @@
 defmodule GildedRoseTest do
   use ExUnit.Case
 
-  test "begin the journey of refactoring" do
-    items = [%Item{name: "foo", sell_in: 0, quality: 0}]
-    GildedRose.update_quality(items)
-    %{name: firstItemName} = List.first(items)
-    assert "fixme" == firstItemName
+  test "Aged Brie: check if quality increases" do
+    items = [%Item{name: "Aged Brie", sell_in: 0, quality: 25}]
+    updated_list = GildedRose.update_quality(items)
+    %{quality: quality} = List.first(updated_list)
+    assert 26 == quality
+  end
+
+  test "Aged Brie: check if quality does not exceed 50" do
+    items = [%Item{name: "Aged Brie", sell_in: 0, quality: 50}]
+    updated_list = GildedRose.update_quality(items)
+    %{quality: quality} = List.first(updated_list)
+    assert 50 == quality
+  end
+
+  test "Aged Brie: check if sell in day decreases" do
+    items = [%Item{name: "Aged Brie", sell_in: 10, quality: 25}]
+    updated_list = GildedRose.update_quality(items)
+    %{sell_in: sell_in} = List.first(updated_list)
+    assert 9 == sell_in
   end
 end
